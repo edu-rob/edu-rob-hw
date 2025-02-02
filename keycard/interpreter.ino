ad8a4d81f181c1cc832945e949184c7717f59751
Alex-2401 <116949593+Alex-2401@users.noreply.github.com>
2025-02-02 10:54:43 +0000

    added words to board LED screen

--- keycard/interpreter.ino
+++ keycard/interpreter.ino
@@ -3,6 +3,7 @@
 #include "interpreter.h"
 #include "common.h"
 
+
 const char COMMAND_DELIMITER = ',';
 const char COMMAND_INSIDE_DELIMITER = ' ';
 
@@ -76,30 +77,57 @@ void CommandInterpreter::send(String message) {
 #ifdef DEBUG
         Serial.println("Forwards");
 #endif
+        display_clear(); // Clear the display
+        display_draw_char('F', 23);
+        display_draw_char('R', 18);
+        display_draw_char('W', 13);
+        display_draw_char('D', 8);
         robot.forwards(value);
         break;
       case Token::BACKWARDS:
 #ifdef DEBUG
         Serial.println("Backwards");
 #endif
+        display_clear(); // Clear the display
+        display_draw_char('B', 23);
+        display_draw_char('K', 18);
+        display_draw_char('W', 13);
+        display_draw_char('D', 8);
         robot.backwards(value);
         break;
       case Token::LEFT:
 #ifdef DEBUG
         Serial.println("Left");
 #endif
+        display_clear(); // Clear the display
+        display_draw_char('L', 23);
+        display_draw_char('E', 18);
+        display_draw_char('F', 13);
+        display_draw_char('T', 9);
         robot.left(value);
         break;
       case Token::RIGHT:
 #ifdef DEBUG
         Serial.println("Right");
 #endif
+        display_clear(); // Clear the display
+        display_draw_char('R', 23);
+        display_draw_char('I', 19);
+        display_draw_char('G', 14);
+        display_draw_char('H', 9);
+        display_draw_char('T', 5);
         robot.right(value);
         break;
       default:
 #ifdef DEBUG
         Serial.println("ERROR OCCURED");
 #endif
+        display_clear(); // Clear the display
+        display_draw_char('E', 23);
+        display_draw_char('R', 18);
+        display_draw_char('R', 13);
+        display_draw_char('O', 8);
+        display_draw_char('R', 3);
         break;
     }
 
